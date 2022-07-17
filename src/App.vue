6459814db66f3f28bf1f1e299da9a226fbe90b39
guangliwoodson <guangliwoodson@outlook.com>
2022-07-17 23:24:30 +0800

    test_2

--- src/App.vue
+++ src/App.vue
@@ -26,46 +26,46 @@
 
             <!-- 如果价格超过100，就有red这个类 -->
             <td :class="{ red: obj.price > 100 }">{{ obj.price }}</td>
-            <td>{{ obj.time }}</td>
-            <td><a href="#">删除</a></td>
+            <td>{{ obj.time | formatDate }}</td>
+            <td><a href="#" @click="delFn(obj.id)">删除</a></td>
           </tr>
-          <!-- <tr style="background-color: #EEE">
-              <td>统计:</td>
-              <td colspan="2">总价钱为: 0</td>
-              <td colspan="2">平均价: 0</td>
-          </tr> -->
+          <tr style="background-color: #EEE">
+            <td>统计:</td>
+            <td colspan="2">总价钱为: {{ allPrice }}</td>
+            <td colspan="2">平均价: {{ avgPrice }}</td>
+          </tr>
         </tbody>
-        <!-- 
-        <tfoot >
+
+        <tfoot>
           <tr>
             <td colspan="5" style="text-align: center">暂无数据</td>
           </tr>
         </tfoot>
-            -->
       </table>
 
       <!-- 添加资产 -->
       <form class="form-inline" style="display:flex;">
         <div class="form-group">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="资产名称" />
+            <input type="text" class="form-control" placeholder="资产名称" v-model="name" />
           </div>
         </div>
         &nbsp;&nbsp;&nbsp;&nbsp;
         <div class="form-group">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="价格" />
+            <input type="text" class="form-control" placeholder="价格" v-model.number="price" />
           </div>
         </div>
         &nbsp;&nbsp;&nbsp;&nbsp;
-        <!-- 阻止表单提交 -->
-        <button class="btn btn-primary">添加资产</button>
+        <!-- 增加表单提交 -->
+        <button class="btn btn-primary" @click.prevent="addFn">添加资产</button>
       </form>
     </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 // 1. 明确需求
 // 2. 标签+样式+默认数据
 // 3. 下载bootstrap, main.js引入bootstrap.css
@@ -74,15 +74,59 @@
 export default {
   data() {
     return {
-      list: [
-        { id: 100, name: "外套", price: 199, time: new Date('2010-08-12') },
-        { id: 101, name: "裤子", price: 34, time: new Date('2013-09-01') },
-        { id: 102, name: "鞋", price: 25.4, time: new Date('2018-11-22') },
-        { id: 103, name: "头发", price: 19900, time: new Date('2020-12-12') }
-      ],
+      // list: [
+      //   { id: 100, name: "外套", price: 199, time: new Date('2010-08-12') },
+      //   { id: 101, name: "裤子", price: 34, time: new Date('2013-09-01') },
+      //   { id: 102, name: "鞋", price: 25.4, time: new Date('2018-11-22') },
+      //   { id: 103, name: "头发", price: 19900, time: new Date('2020-12-12') }
+      // ],
+      name: '',
+      price: 0,
+      list: JSON.parse(localStorage.getItem('pList')) || []
     };
   },
-
+  methods: {
+    addFn() {
+      if (this.name.trim().length == 0 || this.price == 0) {
+        this.name = ''
+        this.price = 0
+        return alert("请输入完整的信息");
+      }
+      this.list.push({
+        id: this.list[this.list.length - 1].id + 1,
+        name: this.name,
+        price: this.price,
+        time: new Date()
+      })
+      this.name = '',
+        this.price = 0
+    },
+    delFn(id) {
+      let index = this.list.findIndex(item => item.id === id)
+      this.list.splice(index, 1)
+    }
+  },
+  filters: {
+    formatDate(val) {
+      return moment(val).format('YYYY-MM-DD')
+    }
+  },
+  computed: {
+    allPrice() {
+      return this.list.reduce((sum, obj) => sum += obj.price, 0)
+    },
+    avgPrice() {
+      return (this.allPrice / this.list.length).toFixed(2)
+    }
+  },
+  watch: {
+    list: {
+      handler() {
+        localStorage.setItem('pList', JSON.stringify(this.list))
+      },
+      deep: true
+    }
+  }
 };
 </script>
 
